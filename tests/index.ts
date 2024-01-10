bb340c5d18151dadbb60b8ff5ef90f3ad21c8139
root <root@localhost.localdomain>
2024-01-10 14:09:31 -0500

    update: remove old folder

--- tests/index.ts
+++ tests/index.ts
@@ -1,4 +1,5 @@
 import { Container, LXC, LXC_ATTACH, LXC_CREATE, LXC_LOGLEVEL } from "../index";
+import * as os from "os";
 
 console.log(`LXC version(${LXC.version})`);
 
@@ -8,6 +9,15 @@ const c = new Container(name);
 
 c.setConfigItem("lxc.log.file", `./${name}/.log`);
 c.setConfigItem("lxc.log.level", LXC_LOGLEVEL.TRACE.toString());
+
+
+c.setConfigItem("lxc.cgroup.devices.allow", "");
+c.setConfigItem("lxc.cgroup.devices.deny", "");
+
+c.setConfigItem("lxc.idmap", "u 0 100000 65536");
+c.setConfigItem("lxc.idmap", "g 0 100000 65536");
+
+c.setConfigItem("lxc.mount.auto", "cgroup:rw:force");
 
 console.log(c.name);
 
@@ -23,7 +33,7 @@ if (!c.defined) {
 }
 
 
-console.log("started", c.start(0, ["/sbin/init"]));
+console.log("started", c.start(0, ["/sbin/init", "systemd.unified_cgroup_hierarchy=1"]));
 // console.log("started", c.start());
 
 
